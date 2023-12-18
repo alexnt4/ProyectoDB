d4ad0e6da96d389e2ea7a7cd60687b28ddc11c1d
Stiven Henao <stivenhenaoa1@gmail.com>
2023-12-17 21:30:41 -0500

    Implementacion agregar pedido

--- src/main/java/org/example/vista/PantallaPrincipal.java
+++ src/main/java/org/example/vista/PantallaPrincipal.java
@@ -1,12 +1,10 @@
 package org.example.vista;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Objects;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
--- src/main/java/org/example/vista/pedido.java
+++ src/main/java/org/example/vista/pedido.java
@@ -1,8 +1,15 @@
 package org.example.vista;
 
+import org.example.DAO.DAOpedido;
+import org.example.controlador.controlador;
+
 import java.awt.Color;
 import java.awt.Font;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -12,7 +19,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
-public class pedido extends JPanel{
+public class pedido extends JPanel implements ActionListener {
     JTextField T_numTitulo;
     static JTextField T_num;
     JTextField T_facturaTitulo;
@@ -39,14 +46,14 @@ public class pedido extends JPanel{
     JRadioButton actualizar;
     JRadioButton eliminar;
 
-    public pedido(){
+    public pedido() {
         Gui(); //Funcion que genera la vista
         vaciarCampos();
     }
 
-    public void Gui(){
+    public void Gui() {
         setLayout(null);
-        setBounds(350,5,920,650);
+        setBounds(350, 5, 920, 650);
         setBackground(new Color(0, 191, 255));
         //setBackground(new Color(220, 220, 220)); //Activelo cuando quiera ver el panel
         setVisible(false);
@@ -57,7 +64,7 @@ public class pedido extends JPanel{
         tituloVentana.setFont(new Font("arial", 3, 30));
         tituloVentana.setEditable(false);
         tituloVentana.setBorder(new LineBorder(Color.gray));
-        tituloVentana.setBounds(30,15,705,40);
+        tituloVentana.setBounds(30, 15, 705, 40);
         add(tituloVentana);
 
         T_numTitulo = new JTextField();
@@ -65,37 +72,37 @@ public class pedido extends JPanel{
         T_numTitulo.setFont(new Font("arial", 3, 17));
         T_numTitulo.setEditable(false);
         T_numTitulo.setBorder(new LineBorder(Color.gray));
-        T_numTitulo.setBounds(30,80,190,30);
+        T_numTitulo.setBounds(30, 80, 190, 30);
         add(T_numTitulo);
 
-        T_num  = new JTextField();
-        T_num.setBounds(240,80,220,31);
+        T_num = new JTextField();
+        T_num.setBounds(240, 80, 220, 31);
         T_num.setBorder(new LineBorder(Color.gray));
-        add(T_num );
+        add(T_num);
 
         T_facturaTitulo = new JTextField();
         T_facturaTitulo.setText(" FACTURA ");
         T_facturaTitulo.setFont(new Font("arial", 3, 15));
         T_facturaTitulo.setEditable(false);
         T_facturaTitulo.setBorder(new LineBorder(Color.gray));
-        T_facturaTitulo.setBounds(30,130,190,30);
-        add( T_facturaTitulo);
+        T_facturaTitulo.setBounds(30, 130, 190, 30);
+        add(T_facturaTitulo);
 
         T_factura = new JTextField();
-        T_factura.setBounds(240,130,220,31);
+        T_factura.setBounds(240, 130, 220, 31);
         T_factura.setBorder(new LineBorder(Color.gray));
-        add( T_factura);
+        add(T_factura);
 
         T_montoTitulo = new JTextField();
         T_montoTitulo.setText(" MONTO TOTAL");
         T_montoTitulo.setFont(new Font("arial", 3, 15));
         T_montoTitulo.setBorder(new LineBorder(Color.gray));
         T_montoTitulo.setEditable(false);
-        T_montoTitulo.setBounds(30,180,190,30);
+        T_montoTitulo.setBounds(30, 180, 190, 30);
         add(T_montoTitulo);
 
         T_monto = new JTextField();
-        T_monto.setBounds(240,180,220,31);
+        T_monto.setBounds(240, 180, 220, 31);
         T_monto.setBorder(new LineBorder(Color.gray));
         add(T_monto);
 
@@ -104,11 +111,11 @@ public class pedido extends JPanel{
         T_abonoTitulo.setFont(new Font("arial", 3, 15));
         T_abonoTitulo.setBorder(new LineBorder(Color.gray));
         T_abonoTitulo.setEditable(false);
-        T_abonoTitulo.setBounds(30,230,190,30);
+        T_abonoTitulo.setBounds(30, 230, 190, 30);
         add(T_abonoTitulo);
 
         T_abono = new JTextField();
-        T_abono.setBounds(240,230,220,30);
+        T_abono.setBounds(240, 230, 220, 30);
         T_abono.setBorder(new LineBorder(Color.gray));
         add(T_abono);
 
@@ -117,11 +124,11 @@ public class pedido extends JPanel{
         T_fecha_encargoTitulo.setFont(new Font("arial", 3, 15));
         T_fecha_encargoTitulo.setBorder(new LineBorder(Color.gray));
         T_fecha_encargoTitulo.setEditable(false);
-        T_fecha_encargoTitulo.setBounds(490,80,190,30);
+        T_fecha_encargoTitulo.setBounds(490, 80, 190, 30);
         add(T_fecha_encargoTitulo);
 
         T_fecha_encargo = new JTextField();
-        T_fecha_encargo.setBounds(700,80,220,30);
+        T_fecha_encargo.setBounds(700, 80, 220, 30);
         T_fecha_encargo.setBorder(new LineBorder(Color.gray));
         add(T_fecha_encargo);
 
@@ -130,11 +137,11 @@ public class pedido extends JPanel{
         T_fecha_entregaTitulo.setFont(new Font("arial", 3, 15));
         T_fecha_entregaTitulo.setBorder(new LineBorder(Color.gray));
         T_fecha_entregaTitulo.setEditable(false);
-        T_fecha_entregaTitulo.setBounds(490,130,190,30);
+        T_fecha_entregaTitulo.setBounds(490, 130, 190, 30);
         add(T_fecha_entregaTitulo);
 
         T_fecha_entrega = new JTextField();
-        T_fecha_entrega.setBounds(700,130,220,30);
+        T_fecha_entrega.setBounds(700, 130, 220, 30);
         T_fecha_entrega.setBorder(new LineBorder(Color.gray));
         add(T_fecha_entrega);
 
@@ -143,11 +150,11 @@ public class pedido extends JPanel{
         T_medidaTitulo.setFont(new Font("arial", 3, 15));
         T_medidaTitulo.setBorder(new LineBorder(Color.gray));
         T_medidaTitulo.setEditable(false);
-        T_medidaTitulo.setBounds(490,180,190,30);
+        T_medidaTitulo.setBounds(490, 180, 190, 30);
         add(T_medidaTitulo);
 
         T_medida = new JTextField();
-        T_medida.setBounds(700,180,220,30);
+        T_medida.setBounds(700, 180, 220, 30);
         T_medida.setBorder(new LineBorder(Color.gray));
         add(T_medida);
 
@@ -156,29 +163,29 @@ public class pedido extends JPanel{
         T_estadoTitulo.setFont(new Font("arial", 3, 15));
         T_estadoTitulo.setBorder(new LineBorder(Color.gray));
         T_estadoTitulo.setEditable(false);
-        T_estadoTitulo.setBounds(490,230,190,30);
+        T_estadoTitulo.setBounds(490, 230, 190, 30);
         add(T_estadoTitulo);
 
         T_estado = new JTextField();
-        T_estado.setBounds(700,230,220,30);
+        T_estado.setBounds(700, 230, 220, 30);
         T_estado.setBorder(new LineBorder(Color.gray));
         add(T_estado);
 
         listo = new JButton("Listo ");
-        listo.setBounds(755,15,165,40);
-        //listo.addActionListener(this);
+        listo.setBounds(755, 15, 165, 40);
+        listo.addActionListener(this);
         listo.setBorder(new LineBorder(Color.gray));
         listo.setForeground(Color.BLACK); //Color de la letra
-        listo.setFont(new Font("cooper black",2,25));
+        listo.setFont(new Font("cooper black", 2, 25));
         listo.setBorderPainted(true); //Establece si el borde del botón debe ser dibujado o no.
         listo.setContentAreaFilled(true); //Establece si el área de contenido del botón debe ser dibujada o no.
         listo.setFocusPainted(false); //desactivar el efecto de resaltado del texto cuando se hace clic en el botón
-       
+
         add(listo);
 
         insertar = new JRadioButton("Insertar");
         insertar.setBounds(30, 280, 115, 30);
-        insertar.setFont(new Font("arial",1,15));
+        insertar.setFont(new Font("arial", 1, 15));
         insertar.setContentAreaFilled(true); //Le quito el fondo
         insertar.setFocusPainted(false); //Que no quede seleccionada
         insertar.setBorder(new LineBorder(Color.gray));
@@ -186,7 +193,7 @@ public class pedido extends JPanel{
 
         consultar = new JRadioButton("consultar");
         consultar.setBounds(160, 280, 115, 30);
-        consultar.setFont(new Font("arial",1,15));
+        consultar.setFont(new Font("arial", 1, 15));
         consultar.setBorderPainted(false);
         consultar.setFocusPainted(false); //Que no quede seleccionada
         consultar.setContentAreaFilled(true); //Le quito el fondo
@@ -195,7 +202,7 @@ public class pedido extends JPanel{
 
         actualizar = new JRadioButton("actualizar");
         actualizar.setBounds(285, 280, 115, 30);
-        actualizar.setFont(new Font("arial",1,15));
+        actualizar.setFont(new Font("arial", 1, 15));
         actualizar.setBorderPainted(false);
         actualizar.setFocusPainted(false); //Que no quede seleccionada
         actualizar.setContentAreaFilled(true); //Le quito el fondo
@@ -204,7 +211,7 @@ public class pedido extends JPanel{
 
         eliminar = new JRadioButton("eliminar");
         eliminar.setBounds(415, 280, 115, 30);
-        eliminar.setFont(new Font("arial",1,15));
+        eliminar.setFont(new Font("arial", 1, 15));
         eliminar.setBorderPainted(false);
         eliminar.setFocusPainted(false); //Que no quede seleccionada
         eliminar.setContentAreaFilled(true); //Le quito el fondo
@@ -221,7 +228,7 @@ public class pedido extends JPanel{
         Tablero.setFont(new Font("arial", 2, 15));
         Tablero.setEditable(true);
         barraDesplazamiento = new JScrollPane(Tablero);
-        barraDesplazamiento.setBounds(30,380,890,250);
+        barraDesplazamiento.setBounds(30, 380, 890, 250);
         barraDesplazamiento.setBorder(new LineBorder(Color.gray));
         add(barraDesplazamiento);
     }
@@ -234,6 +241,40 @@ public class pedido extends JPanel{
         T_estado.setText("");
         Tablero.setText("");
         bg.clearSelection();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == listo) {
+            try {
+                if (insertar.isSelected()) {
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    int numPedido = Integer.parseInt(T_num.getText());
+                    Double abono = Double.valueOf(T_abono.getText());
+                    String medPersona = T_medida.getText();
+                    String estado = T_estado.getText();
+                    int DocCliente = 10001;
+                    int facVenta = Integer.parseInt(T_factura.getText());
+                    Double MontoTotal = Double.valueOf(T_monto.getText());
+                    Date fechaEncargo = new Date(sdf.parse(T_fecha_encargo.getText()).getTime());
+                    Date fechaEntrega = new Date(sdf.parse(T_fecha_entrega.getText()).getTime());
+                    controlador control = new controlador();
+                    controlador.agregarPedido(numPedido, fechaEncargo, abono, fechaEntrega, medPersona, estado, DocCliente, facVenta, MontoTotal);
+                    System.out.println("insertar");
+                    Tablero.setText("Usuario agregado");
+                } else if (actualizar.isSelected()) {
+                    System.out.println("actualizar");
+                } else if (consultar.isSelected()) {
+                    System.out.println("consultar");
+                } else if (eliminar.isSelected()) {
+                    System.out.println("eliminar");
+                } else {
+                    System.out.println("Escoja una opción.");
+                }
+
+            } catch (ParseException f) {
+                f.printStackTrace();
+            }
+        }
     }
 }
 
