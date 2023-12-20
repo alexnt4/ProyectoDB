72c6f9bc1650cda7f3a7ecdb4c495606421d0a45
StivenHenao <142904664+StivenHenao@users.noreply.github.com>
2023-12-20 01:10:17 -0500

    Entrega final, proyecto listo
    Fue un placer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,8 @@ import org.example.vista.PantallaPrincipal;
 
 public class Main {
     public static void main(String[] args) {
-        //new Login();
-        new PantallaPrincipal("administrador","Schwarzenegger"); //Activalo para pruebas y no tener que loguearse
+        new Login();
+        //new PantallaPrincipal("administrador","Schwarzenegger"); //Activalo para pruebas y no tener que loguearse
         //new PantallaPrincipal("vendedor", "Bill Gates");//Si quiero entrar como vendedor
     }
 }
